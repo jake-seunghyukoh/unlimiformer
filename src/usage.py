3776e184752be74180e3e1f95ca5c098d130d9d4
Seunghyuk Oh <jake@seunghyukoh.com>
2023-08-24 10:49:18 +0900

    Fix import

--- src/usage.py
+++ src/usage.py
@@ -1,9 +1,6 @@
 from dataclasses import dataclass, field
 from typing import List, Optional
 
-from random_training_unlimiformer import RandomTrainingUnlimiformer
-from unlimiformer import Unlimiformer
-
 
 @dataclass
 class UnlimiformerArguments:
@@ -65,31 +62,3 @@ class UnlimiformerArguments:
     reconstruct_embeddings: Optional[bool] = field(default=False)
     gpu_datastore: Optional[bool] = field(default=True)
     gpu_index: Optional[bool] = field(default=True)
-
-
-# include these lines in your code somewhere before model training
-def training_addin():
-    if knn_args.test_unlimiformer:
-        unlimiformer_kwargs = {
-            "layer_begin": unlimiformer_args.layer_begin,
-            "layer_end": unlimiformer_args.layer_end,
-            "unlimiformer_head_num": unlimiformer_args.unlimiformer_head_num,
-            "exclude_attention": unlimiformer_args.unlimiformer_exclude,
-            "chunk_overlap": unlimiformer_args.unlimiformer_chunk_overlap,
-            "model_encoder_max_len": unlimiformer_args.unlimiformer_chunk_size,
-            "verbose": unlimiformer_args.unlimiformer_verbose,
-            "tokenizer": tokenizer,
-            "unlimiformer_training": unlimiformer_args.unlimiformer_training,
-            "use_datastore": unlimiformer_args.use_datastore,
-            "flat_index": unlimiformer_args.flat_index,
-            "test_datastore": unlimiformer_args.test_datastore,
-            "reconstruct_embeddings": unlimiformer_args.reconstruct_embeddings,
-            "gpu_datastore": unlimiformer_args.gpu_datastore,
-            "gpu_index": unlimiformer_args.gpu_index,
-        }
-        if unlimiformer_args.random_unlimiformer_training:
-            model = RandomTrainingUnlimiformer.convert_model(
-                model, **unlimiformer_kwargs
-            )
-        else:
-            model = Unlimiformer.convert_model(model, **unlimiformer_kwargs)
